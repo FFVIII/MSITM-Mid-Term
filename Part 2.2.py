34033e3c2a3310d3f6a26bd16d3b1e0bf9e8833e
FFVIII <fangvsyu@gmail.com>
2025-04-09 12:00:05 -0500

    upload part 2.1 and 2.2 code

---
+++ Part 2.2.py
@@ -0,0 +1,6 @@
+# 2.2 Perform Exploratory Data Analysis (EDA) and Visualize Key Insights
+#
+#   (a) Exploratory Data Analysis
+#       - Compute basic stats (mean, median, std, etc.).
+#       - Identify correlations, outliers, or data imbalances.
+#       - Use pandas describe(), info(), corr() for an overview.
